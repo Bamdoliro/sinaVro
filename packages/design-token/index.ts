85fd93186ff7510002cd03b22a1a9277d0d1f65b
junghongseop <a01082372487@gmail.com>
2024-06-02 20:36:31 +0900

    chore: 색상 디자인 시스템 구축

---
+++ packages/design-token/index.ts
@@ -0,0 +1,2 @@
+export {default as color} from './src/color';
+export {default as font} from './src/font';
---
+++ packages/design-token/src/color.ts
@@ -0,0 +1,22 @@
+const color = {
+  gray900: '#191919',
+  gray800: '#333333',
+  gray700: '#4D4D4D',
+  gray600: '#666666',
+  gray500: '#808080',
+  gray400: '#999999',
+  gray300: '#B3B3B3',
+  gray200: '#CCCCCC',
+  gray100: '#E6E6E6',
+  gray50: '#F2F2F2',
+
+  glass_white: 'rgba(255, 255, 255, 0.3)',
+  glass_dark: 'rgba(229, 229, 229, 0.35)',
+
+  white100: '#FFFFFF',
+  white80: 'rgba(255, 255, 255, 0.8)',
+
+  primary: '#FF7433',
+};
+
+export default color;
