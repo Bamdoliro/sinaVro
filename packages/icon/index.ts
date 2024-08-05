3b96b0893b84df05f484d26e55830ffa13fb2b58
junghongseop <a01082372487@gmail.com>
2024-08-06 00:36:50 +0900

    feat: 감정 아이콘 컴포넌트 개발

---
+++ packages/icon/index.ts
@@ -0,0 +1,8 @@
+export { default as IconPleasure } from './src/IconPleasure';
+export { default as IconSad } from './src/IconSad';
+export { default as IconCommon } from './src/IconCommon';
+export { default as IconHappiness } from './src/IconHappiness';
+export { default as IconDepressed } from './src/IconDepressed';
+export { default as IconAnger } from './src/IconAnger';
+export { default as IconEmbarrassment } from './src/IconEmbarrassment';
+export { default as IconAmazed } from './src/IconAmazed';
---
+++ packages/icon/src/IconAnger.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+interface Props {
+  width: number;
+  height: number;
+}
+
+const IconAnger = ({ width, height }: Props) => {
+  return (
+    <StyledIcon source={require('../public/anger.png')} width={width} height={height} />
+  );
+};
+
+export default IconAnger;
+
+const StyledIcon = styled.Image<Props>`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+`;
